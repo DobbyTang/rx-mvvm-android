5a7ed9d8ead78806fb4a9c3ed5b551718fe3c65f
TPJ <514820184@qq.com>
2016-11-10 16:13:28 +0800

    init

---
+++ Libs/Mvvmlibs/src/main/java/com/rx/mvvmlibs/IModel.java
@@ -0,0 +1,17 @@
+package com.rx.mvvmlibs;
+
+import retrofit2.Call;
+
+/**
+ * @ClassName: IModel
+ * @author create by Tang
+ * @date date 16/11/10 上午11:50
+ * @Description: MVVM的model接口，
+ */
+
+public interface IModel {
+
+    void enqueue(Call call);
+
+    void cancel();
+}
---
+++ Libs/Mvvmlibs/src/main/java/com/rx/mvvmlibs/Model.java
@@ -0,0 +1,24 @@
+package com.rx.mvvmlibs;
+
+import retrofit2.Call;
+
+/**
+ * @ClassName: Model
+ * @author create by Tang
+ * @date date 16/11/10 下午2:11
+ * @Description: MVVM的Model实现
+ */
+public class Model implements IModel{
+
+
+
+    @Override
+    public void enqueue(Call call) {
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
